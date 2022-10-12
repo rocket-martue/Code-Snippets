33b561b85145e702d526fa5fa38c30fab3a6bbf8
Toshiyuki Honda <rocket-martue@users.noreply.github.com>
2022-10-12 16:23:57 +0900

    Update archive-introduction.php

--- archive-introduction.php
+++ archive-introduction.php
@@ -7,45 +7,47 @@
  * @param mixed $desc　再利用ブロックの slug
  * @param mixed $link_btn 再利用ブロックの slug
  */
-if ( ! function_exists( 'the_rubi_content' ) ) {
-	add_action(
-		'snow_monkey_before_archive_entry_content',
-		function() {
-			$_post_type = get_post_type();
-			$desc       = null;
-			$link_btn   = null;
-			switch ( $_post_type ) {
-				case 'post':
-					if ( ! is_category() ) {
-						$desc = 'blog-desc';
-					}
-					$link_btn = '';
-					break;
-				case 'news':
-					$desc     = 'news-desc';
-					$link_btn = 'btn-news-link';
-					break;
-				case 'collection':
-					$desc = 'collection-desc';
-					break;
-				case 'gallary':
-					$desc = 'gallary-desc';
-					break;
-			}
-			if ( $desc && ! is_search() ) {
-				?>
-	<div class="desc">
-		<?php the_rubi_content( $desc ); ?>
-	</div>
-				<?php
-			}
-			if ( $link_btn && ! is_search() ) {
+add_action(
+	'snow_monkey_before_archive_entry_content',
+	function() {
+		$_post_type = get_post_type();
+		$desc       = null;
+		$link_btn   = null;
+		switch ( $_post_type ) {
+			case 'post':
+				if ( ! is_category() ) {
+					$desc = 'blog-desc';
+				}
+				$link_btn = '';
+				break;
+			case 'news':
+				$desc     = 'news-desc';
+				// $link_btn = 'btn-news-link';
+				break;
+			case 'collection':
+				$desc = 'collection-desc';
+				break;
+			case 'gallary':
+				$desc = 'gallary-desc';
+				break;
+		}
+		if ( $desc && ! is_search() ) {
+			if ( function_exists( 'the_rubi_content' ) ) {
 			?>
-	<div class="link_btn">
-		<?php the_rubi_content( $link_btn ); ?>
-	</div>
+<div class="desc">
+	<?php the_rubi_content( $desc ); ?>
+</div>
 			<?php
 			}
 		}
-	);
-}
+		if ( $link_btn && ! is_search() ) {
+			if ( function_exists( 'the_rubi_content' ) ) {
+		?>
+<div class="link_btn">
+	<?php the_rubi_content( $link_btn ); ?>
+</div>
+		<?php
+			}
+		}
+	}
+);
